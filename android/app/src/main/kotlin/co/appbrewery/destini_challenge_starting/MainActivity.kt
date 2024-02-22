798968c08783b2fad3fe192987e1c697ce818625
GABRIEL GIDEON BASSEY <140850609+Famous-guy@users.noreply.github.com>
2024-02-22 04:25:48 +0100

    first commit

---
+++ android/app/src/main/kotlin/co/appbrewery/destini_challenge_starting/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.appbrewery.destini_challenge_starting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,21 @@
+//TODO: Step 2 - Create a new class called Story.
+import 'dart:core';
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story(
+      {required this.storyTitle,
+      required this.choice1,
+      required this.choice2}) {
+    // storyTitle = storyTitle;
+    // choice1 = choice1;
+    // choice2 = choice2;
+  }
+}
+
+//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
